2f2882f085a39d9ffe43391820d1cef0bc630981
pricheska <den-ryab2019@yandex.ru>
2022-12-07 17:58:53 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int cost = 16_698;
+
+
+        int miles = cost / 20;
+        System.out.println("Бонусные мили: " + miles);
+    }
+}
