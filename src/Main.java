791c8c240e527d4f1c13c2b7a4988b07ee2c0750
pricheska <den-ryab2019@yandex.ru>
2022-12-08 16:44:37 +0300

    New class

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
 public class Main {
-
     public static void main(String[] args) {
-        int cost = 16_698;
-
-
-        int miles = cost / 20;
-        System.out.println("Бонусные мили: " + miles);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
+
+//public class Main {
+
+  //  public static void main(String[] args) {
+    //    int cost = 16_698;
+
+
+      //  int miles = cost / 20;
+        //System.out.println("Бонусные мили: " + miles);
+    //}
+//}
